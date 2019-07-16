43a392e6b595dae4af0ba51e794c38891d5e31d6
Hayk Ghonakhchyan <hayk.ghonakhchyan@EFU.local>
2019-07-16 17:39:52 +0400

    Deployment mode

---
+++ docs/precache-manifest.4c93e96adefb99d907116455a36de4c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98de912ed25c71f3b98474f2c88dc06",
+    "url": "/Movies-App/index.html"
+  },
+  {
+    "revision": "29aca48227c785b1f0cb",
+    "url": "/Movies-App/static/css/main.1382131e.chunk.css"
+  },
+  {
+    "revision": "395a1e7ad642a0848abd",
+    "url": "/Movies-App/static/js/2.6cfbddcf.chunk.js"
+  },
+  {
+    "revision": "29aca48227c785b1f0cb",
+    "url": "/Movies-App/static/js/main.da5d254c.chunk.js"
+  },
+  {
+    "revision": "ed8df1de7f16c36d8966",
+    "url": "/Movies-App/static/js/runtime~main.8d416b0d.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e15b8684d8e58f64db8bc41b02b52a4.js"
+  "/Movies-App/precache-manifest.4c93e96adefb99d907116455a36de4c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Movies-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
